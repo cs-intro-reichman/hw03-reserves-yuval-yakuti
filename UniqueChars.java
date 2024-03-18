e61350cd37e2710b557a5a33eede664a5b1f0e91
yuval-yakuti <154499779+yuval-yakuti@users.noreply.github.com>
2024-03-18 12:02:00 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,10 +1,12 @@
-/** String processing exercise 2. */
+/** String processing exercise 2. */ 
 public class UniqueChars {
     public static void main(String[] args) {  
-        String str = args[0];
+        String str = "";
+        if (args.length > 0){
+            str = args[0];
+        }
         System.out.println(uniqueChars(str));
     }
-
     /**
      * Returns a string which is identical to the original string, 
      * except that all the duplicate characters are removed,
@@ -12,6 +14,15 @@ public class UniqueChars {
      */
     public static String uniqueChars(String s) {
         // Replace the following statement with your code
-        return null;
+        String ans = "";
+        int i = 0;
+        while (i < s.length()){
+            char current = s.charAt(i);
+            if ((s.indexOf(current) == i) || (current == ' ') ){
+                ans = ans + current;
+            }
+            i++;
+        }
+        return ans;
     }
 }
